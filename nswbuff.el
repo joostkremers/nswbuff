e52ae99a45fde46871e3bcc3d867b4ea1bdf2aa9
Joost Kremers <joostkremers@fastmail.fm>
2019-02-11 09:21:18 +0100

    Update Commentary section.

--- nswbuff.el
+++ nswbuff.el
@@ -33,10 +33,30 @@
 
 ;;; Commentary:
 ;;
-;; This package provides the commands `nswbuff-switch-to-next-buffer'
-;; and `nswbuff-switch-to-previous-buffer' to respectively switch to
-;; the next or previous buffer in the buffer list.
+;; `nswbuff' is an Emacs library for quick buffer switching based on swbuff.el,
+;; incorporating the changes from swbuff-x.el and a few new features not present
+;; in either.
 ;;
+;; This package provides two commands: `nswbuff-switch-to-next-buffer' and
+;; `nswbuff-switch-to-previous-buffer' to switch to the next or previous buffer
+;; in the buffer list, respectively.  During buffer switching, the list of
+;; switchable buffers is visible at the bottom of the frame.
+;;
+;; Switching buffers pops-up a status window at the bottom of the selected
+;; window.  The status window shows the list of switchable buffers where the
+;; switched one is highlighted using `nswbuff-current-buffer-face'.  This window
+;; is automatically discarded after any command is executed or after the delay
+;; specified by `nswbuff-clear-delay'.
+;;
+;; The bufferlist is sorted by how recently the buffers were used.  If you
+;; prefer a fixed (cyclic) order set `nswbuff-recent-buffers-first' to nil.
+;;
+;; When the status window disappears because of the clear-delay you still stay
+;; in switching mode.  The timeout is only a visual thing.  If you want it to
+;; have the same effect as using the buffer, set
+;; `nswbuff-clear-delay-ends-switching' to t.
+;;
+;; There are several options to manipulate the list of switchable buffers.
 ;; The option `nswbuff-exclude-buffer-regexps' defines a list of regular
 ;; expressions for excluded buffers.  The default setting excludes
 ;; buffers whose name begin with a blank character.  To exclude all the
@@ -55,32 +75,15 @@
 ;; You can further customize the list of switchable buffers by setting the
 ;; option `nswbuff-buffer-list-function' to a function that returns a list of
 ;; buffers.  Only the buffers returned by this function will be offered for
-;; switching.  Note that this list is still checked against
+;; switching.  If this function returns nil, the buffers returned by
+;; `buffer-list' is used instead.  Note that this list is still checked against
 ;; `nswbuff-exclude-buffer-regexps', `nswbuff-exclude-mode-regexp' and
-;; `nswbuff-include-buffer-regexps', so set these to `nil' if you do not want
-;; this.  If `nswbuff-buffer-list-function' is `nil' or if its function returns
-;; `nil', the list of buffers returned by the function `buffer-list' is used.
+;; `nswbuff-include-buffer-regexps'.
 ;;
 ;; One function already provided that makes use of this option is
 ;; `nswbuff-projectile-buffer-list', which returns the buffers of the current
 ;; [Projectile](http://batsov.com/projectile/) project plus any buffers in
 ;; `(buffer-list)' that match `nswbuff-include-buffer-regexps'.
-;;
-;; Switching buffers pops-up a status window at the bottom of the
-;; selected window.  The status window shows the list of switchable
-;; buffers where the switched one is hilighted using
-;; `nswbuff-current-buffer-face'.  This window is automatically
-;; discarded after any command is executed or after the delay
-;; specified by `nswbuff-clear-delay'.
-;;
-;; The bufferlist is sorted by how recently the buffers were used.  If
-;; you prefer a fixed (cyclic) order set `nswbuff-recent-buffers-first'
-;; to nil.
-;;
-;; When the status window disappears because of the clear-delay you
-;; still stay in switching mode.  The timeout is only a visual
-;; thing.  If you want it to have the same effect as using the buffer,
-;; set `nswbuff-clear-delay-ends-switching' to t.
 
 ;;; History:
 ;;
